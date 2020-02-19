5bd61fd603b7182f251faabd1b04ae0d5a45233c
gustavo <gustavo.a.jesus@ericssoninovacao.com.br>
2020-02-19 13:49:05 -0300

    projetos inciais

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+n1 = float( raw_input("digite o primeiro numero :"))
+n2 = float( raw_input("digite o segundo numero :" ))
+
+
+soma = n1 + n2
+subtracao =  n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+
+print ("soma : " +  str(soma))
+print ("subtracao : " +  str(subtracao))
+print ("divisao : " +  str(divisao))
+print (" multiplicacao : " +  str(multiplicacao))
+
+
+
+
